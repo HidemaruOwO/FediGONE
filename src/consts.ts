7e3384b85f00b5eb90039bf388afd38aaa34dcf3
HidemaruOwO <owner@v-sli.me>
2025-01-28 14:47:07 +0900

    ✨ to be treated as a generic web page (src/consts.ts, src/layouts/Layout.astro, src/pages...)

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,10 @@
-export const DOMAIN = "mi.v-sli.me";
+export const DOMAIN = "ap.example.tld";
+export const TITLE = "インスタンスの墓標";
 export const DESCRIPTION = `このインスタンスは閉鎖しました。"${DOMAIN}"を支えていただきありがとうございました。`;
+export const MAIN_MESSAGE_LINE: string[] = [
+	`このインスタンスは滅んでしまったようです。`,
+	`ここにあるのは、`,
+	`インスタンスの墓標です。`,
+];
+export const NEXT_INSTANCE_URL = "https://new.example.tld";
+export const NEXT_INSTANCE_MESSAGE = "新しいインスタンスに進む";
